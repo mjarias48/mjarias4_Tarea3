37391a6903a96d10ad34074a8d8ab3848d212d2b
mjarias48 <65546768+mjarias48@users.noreply.github.com>
2021-11-24 18:38:26 -0500

    Tarea 3

---
+++ ParteIII/validar.js
@@ -0,0 +1,27 @@
+function validar() {
+	// body...
+	var correo, telefono, expresion;
+	correo= document.getElementById('txtEmail').value;
+	telefono= document.getElementById('txtTelefono').value;
+	expresion= /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+	if(!expresion.test(correo)){
+		alert("El correo no es válido");
+		return false;
+	}
+	
+	else if(telefono.length!=10){
+		alert("El telefono incorrecto");
+		return false;
+	}
+	else if(isNaN(telefono)){
+		alert("El telefono ingresado no es un número");
+		return false;
+	}
+	
+}
+function checkSubmit() {
+    document.getElementById("btnEnviar").value = "Enviando...";
+    document.getElementById("btnEnviar").disabled = true;
+    alert("Datos enviados");
+    return true;
+}
